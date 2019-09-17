3b0a76af80800975ef47c3f9b7dbef1e315d9d03
kuanwei.lin <kuanwei.lin@linecorp.com>
2019-09-17 16:15:58 +0800

    Follow the video on https://youtu.be/gmiEQKlt1fY

--- src/TennisScore.java
+++ src/TennisScore.java
@@ -1,26 +1,69 @@
 import java.util.HashMap;
 
+import static java.lang.Math.abs;
+
 public class TennisScore {
 
+    private final String playerOneName;
+    private final String playerTwoName;
     private int playerOneScore;
     private HashMap<Integer, String> map = new HashMap<Integer, String>() {{
-       put(0, "Love");
-       put(1, "Fifteen");
-       put(2, "Thirty");
-       put(3, "Forty");
+        put(0, "Love");
+        put(1, "Fifteen");
+        put(2, "Thirty");
+        put(3, "Forty");
     }};
-    private int playerTwoScre;
+    private int playerTwoScore;
+
+    public TennisScore(String playerOneName, String playerTwoName) {
+        this.playerOneName = playerOneName;
+        this.playerTwoName = playerTwoName;
+    }
 
     public String score() {
-        if (playerOneScore == playerTwoScre) {
-            return map.get(playerTwoScre) + " All";
-        }
+        return isSamePoint() ?
+                isDeuce() ? deuce() : sameScore() :
+                isReadyForGamePoint() ? advState() : lookupScore();
+    }
 
-        if (playerOneScore > 0) {
-            return map.get(playerOneScore) + " Love";
-        }
+    private String sameScore() {
+        return map.get(playerOneScore) + " All";
+    }
 
-        return "";
+    private String deuce() {
+        return "Deuce";
+    }
+
+    private String advState() {
+        return isAdv() ? String.format("%s Adv", advPlayer()) : String.format("%s Win", advPlayer());
+    }
+
+    private boolean isDeuce() {
+        return playerOneScore >= 3;
+    }
+
+    private boolean isAdv() {
+        return scoreDiff() == 1;
+    }
+
+    private boolean isReadyForGamePoint() {
+        return playerOneScore >= 4 || playerTwoScore >= 4;
+    }
+
+    private String advPlayer() {
+        return playerOneScore > playerTwoScore ? playerOneName : playerTwoName;
+    }
+
+    private String lookupScore() {
+        return map.get(playerOneScore) + " " + map.get(playerTwoScore);
+    }
+
+    private boolean isSamePoint() {
+        return playerOneScore == playerTwoScore;
+    }
+
+    private int scoreDiff() {
+        return abs(playerOneScore - playerTwoScore);
     }
 
     public void playerOneScored() {
@@ -28,6 +71,6 @@ public class TennisScore {
     }
 
     public void playerTwoScored() {
-        playerTwoScre++;
+        playerTwoScore++;
     }
 }
--- src/TennisTests.java
+++ src/TennisTests.java
@@ -4,8 +4,8 @@ import static junit.framework.TestCase.assertEquals;
 
 public class TennisTests {
 
-    private final TennisScore sut = new TennisScore();
-    
+    private final TennisScore sut = new TennisScore("Ace", "Ron");
+
     @Test
     public void Love_All() {
         resultsShouldBe("Love All");
@@ -13,11 +13,124 @@ public class TennisTests {
 
     @Test
     public void fifteen_Love() {
-        sut.playerOneScored();
+        givenPlayerOneScored(1);
         resultsShouldBe("Fifteen Love");
+    }
+
+    @Test
+    public void thirty_Love() {
+        givenPlayerOneScored(2);
+        resultShouldBe("Thirty Love");
+    }
+
+    @Test
+    public void fourty_Love() {
+        givenPlayerOneScored(3);
+        resultShouldBe("Forty Love");
+    }
+
+    @Test
+    public void love_Fifteen() {
+        givenPlayerTwoScored(1);
+        resultShouldBe("Love Fifteen");
+    }
+
+    @Test
+    public void love_Thirty() {
+        givenPlayerTwoScored(2);
+        resultShouldBe("Love Thirty");
+    }
+
+    @Test
+    public void fifteenAll() {
+        givenPlayerOneScored(1);
+        givenPlayerTwoScored(1);
+        resultShouldBe("Fifteen All");
+    }
+
+    @Test
+    public void deuce_3_3() {
+        givenDeuce();
+        resultShouldBe("Deuce");
+    }
+
+    @Test
+    public void deuce_4_4() {
+        givenDeuce();
+        givenPlayerOneScored(1);
+        givenPlayerTwoScored(1);
+        resultShouldBe("Deuce");
+    }
+
+    @Test
+    public void playerOneAdv() {
+        givenDeuce();
+        givenPlayerOneScored(1);
+        resultShouldBe("Ace Adv");
+    }
+
+    @Test
+    public void playerTwoAdv() {
+        givenDeuce();
+        givenPlayerTwoScored(1);
+        resultsShouldBe("Ron Adv");
+    }
+
+    @Test
+    public void score_is_5_3() {
+        givenDeuce();
+        givenPlayerOneScored(2);
+        resultsShouldBe("Ace Win");
+    }
+
+    @Test
+    public void score_is_2_4() {
+        givenPlayerOneScored(2);
+        givenPlayerTwoScored(4);
+        resultsShouldBe("Ron Win");
+    }
+
+
+
+
+
+
+    @Test
+    public void Deuce_3_3() {
+        sut.playerOneScored();
+        sut.playerOneScored();
+        sut.playerOneScored();
+
+        sut.playerTwoScored();
+        sut.playerTwoScored();
+        sut.playerTwoScored();
+
+        resultsShouldBe("Deuce");
+
     }
 
     private void resultsShouldBe(String expected) {
         assertEquals(expected, sut.score());
+    }
+
+    private void givenDeuce() {
+        givenPlayerOneScored(3);
+        givenPlayerTwoScored(3);
+    }
+
+    private void givenPlayerTwoScored(int times) {
+        for (int i = 0; i < times; i++) {
+            sut.playerTwoScored();
+        }
+    }
+
+    private void resultShouldBe(String expected) {
+        assertEquals(expected, sut.score());
+    }
+
+    private void givenPlayerOneScored(int times) {
+        for (int i = 0; i < times; i++) {
+            sut.playerOneScored();
+        }
     }
 }
